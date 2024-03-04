4567dca7cc24ccf79650bf6164adcce9f083a569
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-04 20:00:40 +0300

    Update API service to handle potential null responses

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -4,15 +4,15 @@ const apiBaseEndpoint = process.env.REACT_APP_API_BASE_ENDPOINT;
 
 export const getCharacters = async () => {
   const res = await axios(`${apiBaseEndpoint}/character`);
-  return res.data;
+  return res?.data;
 };
 
 export const getMoreCharacters = async (url) => {
   const res = await axios(url);
-  return res.data;
+  return res?.data;
 };
 
 export const getCharacterDetail = async (id) => {
   const res = await axios(`${apiBaseEndpoint}/character/${id}`);
-  return res.data;
+  return res?.data;
 };
